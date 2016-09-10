8f835cf28feed7a7787264acda02235312f96c1c
Stefanos Chrs <stefanoschrs@hotmail.com>
2016-09-10 23:27:41 +0300

    styling of widget directive

--- public/javascripts/app/controllers/analysisController.js
+++ public/javascripts/app/controllers/analysisController.js
@@ -10,7 +10,29 @@
 	function AnalysisController($scope, $log, Data){
 		$log.debug(Data);
 
-		$scope.charts = Data.map(function(item){
+		$scope.charts = Data.data.map(function(item){
+			item.size = 6;
+
+			item.config = {
+				options: {
+					chart: {
+						type: 'bar'
+					},
+					xAxis: {
+						categories: ['a','b']
+					}
+				},
+				series: [
+					{
+						name: 'Efficient Notifications from Sales',
+						data: [1,2]
+					}
+				],
+				title: {
+					text: ''
+				}
+			};
+
 
 			return item;
 		});
