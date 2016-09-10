e239a0f2466d37d722c0cf9b24b32d151e3b1787
Stefanos Chrs <stefanoschrs@hotmail.com>
2016-09-10 23:57:23 +0300

    First chart!!! Hooray

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -4,6 +4,7 @@
 	angular
 		.module('MouflonView', [
 			'ui.router',
-			'ui.bootstrap'
+			'ui.bootstrap',
+			'highcharts-ng'
 		]);
 })();
--- public/javascripts/app/controllers/analysisController.js
+++ public/javascripts/app/controllers/analysisController.js
@@ -8,31 +8,91 @@
 	AnalysisController.$inject = ['$scope', '$log', 'Data'];
 
 	function AnalysisController($scope, $log, Data){
+		var MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
 		$log.debug(Data);
 
-		$scope.charts = Data.data.map(function(item){
+		Data.data[0].config = undefined;
+		Data.data[1].config = undefined;
+		Data.data[2].config = {
+			options: {
+				chart: { type: 'column' },
+				xAxis: { categories: MONTHS }
+			},
+			series: Data.data[2].data.map(function(el){
+				return {
+					name: el.year,
+					data: el.values
+				};
+			}),
+			title: { text: '' }
+		};
+
+		//Data.data[3].config = {
+		//	options: {
+		//		xAxis: [{ categories: extractData0('name') }],
+		//			yAxis: [
+		//			{
+		//				labels: {
+		//					style: {
+		//						color: '#89A54E'
+		//					}
+		//				},
+		//				title: {
+		//					text: 'Efficiency',
+		//					style: {
+		//						color: '#89A54E'
+		//					}
+		//				},
+		//				opposite: true
+		//
+		//			},
+		//			{
+		//				gridLineWidth: 0,
+		//				title: {
+		//					text: 'Efficient Notifications',
+		//					style: {
+		//						color: '#4572A7'
+		//					}
+		//				},
+		//				labels: {
+		//					style: {
+		//						color: '#4572A7'
+		//					}
+		//				}
+		//
+		//			},
+		//			{
+		//				gridLineWidth: 0,
+		//				title: {
+		//					text: 'Push Notifications',
+		//					style: {
+		//						color: '#AA4643'
+		//					}
+		//				},
+		//				labels: {
+		//					style: {
+		//						color: '#AA4643'
+		//					}
+		//				}
+		//			}
+		//		],
+		//			tooltip: {
+		//			shared: true
+		//		}
+		//	},
+		//	series: [
+		//		{ name: 'Push Notifications', 		color: '#4572A7', type: 'column', data: extractData0('pushNotifications'), 		yAxis: 2 },
+		//		{ name: 'Efficient Notifications', 	color: '#AA4643', type: 'column', data: extractData0('efficientNotifications'), yAxis: 1 },
+		//		{ name: 'Efficiency', 				color: '#89A54E', type: 'spline', data: extractData0('efficiency'), 			tooltip: { valueSuffix: ' %' } }
+		//	],
+		//		title: {
+		//	text: ''
+		//}
+
+
+		$scope.charts = Data.data.map(function(item , index){
 			item.size = 6;
-
-			item.config = {
-				options: {
-					chart: {
-						type: 'bar'
-					},
-					xAxis: {
-						categories: ['a','b']
-					}
-				},
-				series: [
-					{
-						name: 'Efficient Notifications from Sales',
-						data: [1,2]
-					}
-				],
-				title: {
-					text: ''
-				}
-			};
-
 
 			return item;
 		});
