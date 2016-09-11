c833ed6ababe2440ce8eca41d5cbfebbc0b7ee32
Stefanos Chrs <stefanoschrs@hotmail.com>
2016-09-11 07:41:52 +0300

    docs view

--- public/javascripts/docs/app.js
+++ public/javascripts/docs/app.js
@@ -12,11 +12,11 @@
 			{
 				title: 'Tourism',
 				endpoints: [
-					{ url: '/api/raw/tourism', name: 'Tourism', description: '' },
-					{ url: '/api/raw/tourism', name: 'Tourism', description: '' },
-					{ url: '/api/raw/tourism', name: 'Tourism', description: '' },
-					{ url: '/api/raw/tourism', name: 'Tourism', description: '' },
-					{ url: '/api/raw/tourism', name: 'Tourism', description: '' }
+					{ type: 'GET', url: '/api/raw/tourism', name: 'Tourism', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur debitis excepturi ipsam laboriosam minima nam officia quam voluptatibus. 1' },
+					{ type: 'GET', url: '/api/raw/tourism', name: 'Tourism', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur debitis excepturi ipsam laboriosam minima nam officia quam voluptatibus. 2' },
+					{ type: 'GET', url: '/api/raw/tourism', name: 'Tourism', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur debitis excepturi ipsam laboriosam minima nam officia quam voluptatibus. 3' },
+					{ type: 'GET', url: '/api/raw/tourism', name: 'Tourism', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur debitis excepturi ipsam laboriosam minima nam officia quam voluptatibus. 4' },
+					{ type: 'GET', url: '/api/raw/tourism', name: 'Tourism', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur debitis excepturi ipsam laboriosam minima nam officia quam voluptatibus. 5' }
 				]
 			}
 		];
